5d5d6c1b0da60e48df876f904de42f5fc0292118
Diegooo <513642@vistacollege.nl>
2024-08-02 13:58:00 +0200

    Feat: Being idiot

--- src/main/java/org/diegooo/servermanagement/commands/ServerManagementCommand.java
+++ src/main/java/org/diegooo/servermanagement/commands/ServerManagementCommand.java
@@ -23,7 +23,7 @@ public class ServerManagementCommand implements CommandExecutor {
         player.sendMessage(CC.chat("&8&m-----------------------------"));
         player.sendMessage(CC.chat("&3&lServer Management Help&7"));
         player.sendMessage(" ");
-        player.sendMessage(CC.chat("&b❀ &7/settings <world> &8&l| &7opens the &bmenu"));
+        player.sendMessage(CC.chat("&b❀ &7/settings &8&l| &7opens the &bmenu"));
         player.sendMessage(CC.chat("&b❀ &7/setspawnworld <world> &8&l| &7sets spawn of &bworlds"));
         player.sendMessage(CC.chat("&b❀ &7/setmaxplayers <amount> &8&l| &7sets &bmax&7 players"));
         player.sendMessage(CC.chat("&b❀ &7/setmotd <motd> &8&l| &7sets the &bserver&7 motd"));
